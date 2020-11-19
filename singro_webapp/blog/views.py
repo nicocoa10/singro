b035158df7ec34bd3366ba51712156fbb3522d78
nicocoa10 <nojeda3@calstatela.edu>
2020-11-19 13:51:43 -0800

    Finished setting up correct views for Comments, and set up a simple super user admin type of login page

--- singro_webapp/blog/views.py
+++ singro_webapp/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404,redirect
+from django.utils import timezone
 from .models import Post , Comment
 from django.views.generic import (TemplateView , ListView,
                                   DetailView,CreateView, UpdateView,DeleteView)
@@ -6,6 +7,7 @@ from django.utils import timezone
 from .forms import PostForm,CommentForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -56,3 +58,45 @@ class DraftListView(LoginRequiredMixin,ListView):
 
     def get_queryset(self): #sql query in python
         return Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+
+
+############## Now Taking Care of the Comments  ###########################
+
+@login_required
+def post_publish(request,pk):
+    post= get_object_or_404(Post,pk=pk)
+    post.publish
+    return  redirect('post_detail',pk=post.pk)
+
+
+
+#using function based views
+@login_required
+def add_comment_to_post(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post=post
+            comment.save()
+            return redirect('post_detail',pk=post.pk)
+
+        else:
+            form = CommentForm()
+        return render(request, 'blog/comment_form.html',{'form':form})
+
+
+@login_required
+def comment_approve(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    comment.approve()
+    return redirect('post_detail',pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    post_pk=comment.post.pk
+    comment.delete()
+    return redirect('post_detail',pk=post_pk)
