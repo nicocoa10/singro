ff0bbb3543b218e2d483e095ac3851086f66cb15
nicocoa10 <nojeda3@calstatela.edu>
2020-11-19 20:15:03 -0800

    templates edited , specifically the post detail template that displays a post , comments , and allows if user is authenticated to add comment , etc.

--- singro_webapp/singro_webapp/urls.py
+++ singro_webapp/singro_webapp/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
-from django.contrib.auth import views
+from django.contrib.auth.views import LoginView,LogoutView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('account/login', views.login,name='login'),
-    path('account/logout', views.logout, name='logout', kwargs={'next_page':'/'} ),
+    path('account/', include('django.contrib.auth.urls')),
+    # path('account/logout', LogoutView.as_view(), name='logout', kwargs={'next_page':'/'} ),
 
 ]
