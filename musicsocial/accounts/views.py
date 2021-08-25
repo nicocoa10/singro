f442009f5f6bba775a38b175bbe3724aa1ee769d
nicocoa10 <nojeda3@calstatela.edu>
2021-08-24 18:56:45 -0700

    profile page developed

--- musicsocial/accounts/views.py
+++ musicsocial/accounts/views.py
@@ -1,12 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse_lazy
+from django.views.generic.base import View
+from django.views.generic.detail import DetailView
 # Create your views here.
 from . import forms
 
 from django.views.generic import CreateView
 
 
+class ShowProfilePageView(LoginRequiredMixin, DetailView):
+
+    model = get_user_model()
+    template_name = 'accounts/user_profile.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ShowProfilePageView,
+                        self).get_context_data(*args, **kwargs)
+        page_user = get_object_or_404(get_user_model(), id=self.kwargs['pk'])
+
+        context['page_user'] = page_user
+        return context
+
+
 class SignUp(CreateView):
     form_class = forms.UserCreateForm
-    success_url = reverse_lazy('login') # once someone has signed up to our website we will reverse them to the login page WHEN they click singup
+    # once someone has signed up to our website we will reverse them to the login page WHEN they click singup
+    success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
--- musicsocial/groups/views.py
+++ musicsocial/groups/views.py
@@ -9,7 +9,7 @@ from groups.models import Group, GroupMember
 
 # Create your views here.
 
-class CreateGroup(LoginRequiredMixin,generic.CreateView):
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
 
     fields = ('name', 'description')
 
@@ -19,46 +19,42 @@ class CreateGroup(LoginRequiredMixin,generic.CreateView):
 class SingleGroup(generic.DetailView):
     model = Group
 
+
 class LisGroup(generic.ListView):
     model = Group
 
 
 class JoinGroup(LoginRequiredMixin, generic.RedirectView):
 
-
     def get_redirect_url(self, *args, **kwargs):
         return reverse('groups:single', kwargs={'slug': self.kwargs.get('slug')})
 
-    def get(self,request, *args , **kwargs):
-        group= get_object_or_404(Group, slug = self.kwargs.get('slug'))
+    def get(self, request, *args, **kwargs):
+        group = get_object_or_404(Group, slug=self.kwargs.get('slug'))
 
         try:
             GroupMember.objects.create(user=self.request.user, group=group)
 
-        except :
-            messages.warning(self.request,'warning already a member!')
+        except:
+            messages.warning(self.request, 'warning already a member!')
         else:
-            messages.success(self.request,'You are now a member!')
+            messages.success(self.request, 'You are now a member!')
 
-        return  super().get(request,*args,**kwargs)
-
-
-
+        return super().get(request, *args, **kwargs)
 
 
 class LeaveGroup(LoginRequiredMixin, generic.RedirectView):
 
-
     def get_redirect_url(self, *args, **kwargs):
         return reverse('groups:single', kwargs={'slug': self.kwargs.get('slug')})
 
-    def get(self,request,*args,**kwargs):
+    def get(self, request, *args, **kwargs):
 
         try:
             membership = GroupMember.objects.filter(
 
-                user = self.request.user,
-                group__slug = self.kwargs.get('slug')
+                user=self.request.user,
+                group__slug=self.kwargs.get('slug')
 
             ).get()
 
@@ -68,8 +64,6 @@ class LeaveGroup(LoginRequiredMixin, generic.RedirectView):
             membership.delete()
             messages.success(self.request, 'You have left the group!')
 
-        return  super().get(request,*args,**kwargs)
-
-
+        return super().get(request, *args, **kwargs)
 
     pass
